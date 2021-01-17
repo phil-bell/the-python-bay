0045720c74aa1b30bfdc2f27d9fba9e00d8f07f2
philhabell <philhabell@gmail.com>
2021-01-17 18:37:48 +0000

    Attempting to add fallback class

--- the_python_bay/main.py
+++ the_python_bay/main.py
@@ -1,6 +1,7 @@
 import urllib
 import requests
-from typing import List
+from bs4 import BeautifulSoup
+from typing import Collection, List
 
 
 class Torrent:
@@ -17,43 +18,113 @@ class Torrent:
 
     @magnet.setter
     def magnet(self, value) -> None:
-        self._magnet = f"magnet:?xt=urn:btih:{value}&dn={urllib.parse.quote(self.name)}{self.trackers}"
+        if "magnet:?xt=urn:btih:" not in value:
+            self._magnet = f"magnet:?xt=urn:btih:{value}&dn={urllib.parse.quote(self.name)}{self.trackers}"
+        else:
+            self._magnet = value
 
     @property
     def to_dict(self):
         return {**self.__dict__, **{"magnet": self.magnet}}
 
+class TheFallbackBay:
+    def __init__(self) -> None:
+        self.site_url = "https://thepiratebay10.org/"
+        self.search_url = "search/"
+        self.sfw_filter = "/1/99/100,200,300,400,600"
+
+    @classmethod
+    def search_dict(cls, search_term: str, sfw=False) -> list:
+        tpb = cls()
+        res = requests.get(
+            f"{tpb.site_url}{tpb.search_url}{urllib.parse.quote(search_term)}{tpb.sfw_filter if sfw else ''}"
+        )
+        soup = BeautifulSoup(res.content, "html.parser")
+        rows = soup.find_all("tr")
+        torrents = []
+        for row in rows[1:]:
+            cols = [*row.children]
+            if len(cols) > 3:
+                torrents += [{
+                    "name": cols[3].div.a.string,
+                    "magnet": cols[3].a["href"],
+                    "seeders": cols[5].string,
+                    "username": cols[3].font.a and cols[3].font.a.string,
+                    "status": cols[3].a.img and cols[3].a.img.get("title", None)
+                }]
+        return torrents
+
+    @classmethod
+    def search(cls, search_term: str, sfw=False) -> list:
+        tpb = cls()
+        res = requests.get(
+            f"{tpb.site_url}{tpb.search_url}{urllib.parse.quote(search_term)}{tpb.sfw_filter if sfw else ''}"
+        )
+        soup = BeautifulSoup(res.content, "html.parser")
+        rows = soup.find_all("tr")
+        torrents = []
+        for row in rows[1:]:
+            cols = [*row.children]
+            if len(cols) > 3:
+                torrents += [Torrent({
+                    "name": cols[3].div.a.string,
+                    "magnet": cols[3].a["href"],
+                    "seeders": cols[5].string,
+                    "username": cols[3].font.a and cols[3].font.a.string,
+                    "status": cols[3].a.img and cols[3].a.img.get("title", None)
+                })]
+        return torrents
 
 class ThePythonBay:
     def __init__(self) -> None:
         self.api_url = "https://apibay.org/"
         self.search_url = "q.php?q="
         self.sfw_filter = "&cat=100,200,300,400,600"
+        self._response = {}
+
+    @property
+    def response(self):
+        return self._response
+
+    @response.setter
+    def response(self, value):
+        self._response = value
+
+    @property
+    def fallback(self):
+        torrents = self.response.json()
+        return self.repsonse.status != 200 or not torrents or torrents[0]["seeders"] == 0
+
 
     @classmethod
     def search(cls, search_term: str, sfw=False) -> List[Torrent]:
         tpb = cls()
-        res = requests.get(
+        tpb.response = requests.get(
             f"{tpb.api_url}{tpb.search_url}{urllib.parse.quote(search_term)}{tpb.sfw_filter if sfw else ''}"
         )
-        return [Torrent(torrent) for torrent in res.json()]
+        if tpb.fallback:
+            return TheFallbackBay.search(search_term)
+        return [Torrent(torrent) for torrent in tpb.response.json()]
 
     @classmethod
     def search_dict(cls, search_term: str, sfw=False) -> list:
         tpb = cls()
-        res = requests.get(
+        tpb.response = requests.get(
             f"{tpb.api_url}{tpb.search_url}{urllib.parse.quote(search_term)}{tpb.sfw_filter if sfw else ''}"
         )
-        return [Torrent(torrent).to_dict for torrent in res.json()]
+        if tpb.fallback:
+            return TheFallbackBay.search(search_term)
+        return [Torrent(torrent).to_dict for torrent in tpb.response]
 
     @classmethod
     def top_movies(cls) -> List[Torrent]:
         tpb = cls()
-        res = requests.get(f"{tpb.api_url}precompiled/data_top100_201.json")
-        return [Torrent(torrent) for torrent in res.json()]
+        tpb.response = requests.get(f"{tpb.api_url}precompiled/data_top100_201.json")
+        return [Torrent(torrent) for torrent in tpb.response]
 
     @classmethod
     def top_tv(cls) -> List[Torrent]:
         tpb = cls()
-        res = requests.get(f"{tpb.api_url}precompiled/data_top100_205.json")
-        return [Torrent(torrent) for torrent in res.json()]
+        tpb.response = requests.get(f"{tpb.api_url}precompiled/data_top100_205.json")
+        return [Torrent(torrent) for torrent in tpb.response]
+
--- the_python_bay/tests/test_the_python_bay.py
+++ the_python_bay/tests/test_the_python_bay.py
@@ -1,5 +1,6 @@
 import json
 from ..main import ThePythonBay as tpb
+from ..main import TheFallbackBay as tfb
 
 
 class TestThePythonBay:
@@ -7,12 +8,11 @@ class TestThePythonBay:
         results = tpb.search("ubuntu")
         assert len(results) > 1
 
-    def test_the_python_bay_search_json(self):
-        results = tpb.search_json("ubuntu")
+    def test_the_python_bay_search_dict(self):
+        results = tpb.search_dict("ubuntu")
         assert len(results) > 1
         first = results[0]
-        first_dict = json.loads(first)
-        assert "magnet" in first_dict.keys()
+        assert "magnet" in first.keys()
 
     def test_the_python_bay_top_movies(self):
         top_movies = tpb.top_movies()
@@ -27,3 +27,15 @@ class TestTorrent:
     def test_name(self):
         torrent = tpb.search("ubuntu")[0]
         assert "ubuntu" in torrent.name.lower()
+
+
+class TestFallbackBay:
+    def test_magnet(self):
+        torrent = tfb.search_dict("ubuntu")[0]
+        assert "ubuntu" in torrent.name.lower()
+
+    def test_magnet(self):
+        torrent = tfb.search_dict("ubuntu")[0]
+        assert "magnet" in torrent.keys()
+
+
